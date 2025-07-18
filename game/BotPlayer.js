3b7c546741079b2093d534b7170176f9c5789cce
mmcmillan1999 <matthewgmcmillan@yahoo.com>
2025-07-17 21:05:48 -0600

    Bot 3

--- game/BotPlayer.js
+++ game/BotPlayer.js
@@ -1,5 +1,6 @@
 const gameLogic = require('./logic');
 const { RANKS_ORDER } = require('./constants');
+const { getLegalMoves } = require('./legalMoves'); // --- MODIFIED: Import the new logic ---
 
 // Helper function to get the rank value of a card for sorting.
 const getRankValue = (card) => RANKS_ORDER.indexOf(gameLogic.getRank(card));
@@ -33,37 +34,44 @@ class BotPlayer {
         const hand = this.table.hands[this.playerName];
         if (!hand || hand.length === 0) return;
 
-        // 1. Determine the set of legally playable cards.
-        const leadSuit = this.table.leadSuitCurrentTrick;
-        let legalPlays = hand;
-        if (leadSuit) {
-            const cardsInLeadSuit = hand.filter(c => gameLogic.getSuit(c) === leadSuit);
-            if (cardsInLeadSuit.length > 0) {
-                legalPlays = cardsInLeadSuit;
-            }
+        // --- MODIFICATION: The entire card selection logic is now refactored ---
+
+        // 1. Get all legal moves first. This prevents the bot from ever getting stuck.
+        const isLeading = this.table.currentTrickCards.length === 0;
+        const legalPlays = getLegalMoves(
+            hand,
+            isLeading,
+            this.table.leadSuitCurrentTrick,
+            this.table.trumpSuit,
+            this.table.trumpBroken
+        );
+
+        // If for some reason there are no legal plays, exit to prevent a crash.
+        if (legalPlays.length === 0) {
+            console.error(`[${this.table.tableId}] Bot ${this.playerName} has no legal moves from hand: ${hand.join(', ')}`);
+            return;
         }
 
-        // 2. Sort legal cards from lowest rank to highest.
+        // 2. Sort the legal cards from lowest rank to highest.
         legalPlays.sort((a, b) => getRankValue(a) - getRankValue(b));
 
-        // 3. Apply playing strategy.
+        // 3. Apply the playing strategy you outlined.
         let cardToPlay;
-        if (this.table.currentTrickCards.length === 0) {
-            // LOGIC A: I am leading the trick. Play my highest card.
+        if (isLeading) {
+            // LOGIC A: I am leading the trick. Play my highest legal card.
             cardToPlay = legalPlays[legalPlays.length - 1];
         } else {
             // LOGIC B: I am not leading.
-            const currentWinningPlay = gameLogic.determineTrickWinner(this.table.currentTrickCards, leadSuit, this.table.trumpSuit);
-            
             // Find which of my legal cards can win the trick.
             const winningPlays = legalPlays.filter(myCard => {
                 const potentialTrick = [...this.table.currentTrickCards, { card: myCard, userId: this.userId }];
-                const winner = gameLogic.determineTrickWinner(potentialTrick, leadSuit, this.table.trumpSuit);
+                const winner = gameLogic.determineTrickWinner(potentialTrick, this.table.leadSuitCurrentTrick, this.table.trumpSuit);
                 return winner.userId === this.userId;
             });
 
             if (winningPlays.length > 0) {
                 // I can win! Play the highest card I can win with.
+                winningPlays.sort((a, b) => getRankValue(a) - getRankValue(b));
                 cardToPlay = winningPlays[winningPlays.length - 1];
             } else {
                 // I cannot win. Play my lowest legal card to save good cards.
